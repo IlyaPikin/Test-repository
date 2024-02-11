0cf3d92f9f12c2cc1479e733c104f46e27afe675
IlyaPikin <ilya.p@hotmail.com>
2024-02-11 13:55:02 +0300

    The first line of my commit message.
    The next text of my commit message.
    I added some includes in file.cpp and output
    the simple example.

--- file.cpp
+++ file.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
 using namespace std;
 
 int main(){
-	cout<<"Hello world!";
+	cout<<"Hello world!\n";
+	cout << "2 + 2 = 4";
 	return 0;
 }
