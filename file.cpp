2fa9d7a28d9c5a1ce4709b59fb27e3cfc21334ed
IlyaPikin <ilya.p@hotmail.com>
2024-02-12 15:36:34 +0300

    intermediate commit

--- file.cpp
+++ file.cpp
@@ -4,8 +4,15 @@
 #include <string>
 using namespace std;
 
+static int counter = 0;
+
 int main(){
 	cout<<"Hello world!!!\n";
-	cout << "2 + 2 = 4";
+	cout << "2 + 2 != 5";
+
+	vector<int> a = {1, 1, 2, 3, 5, 8};
+
 	return 0;
 }
+
+void print(const vector<int> &vec);
